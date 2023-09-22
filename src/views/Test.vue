001ff0ae52a7d65365a1288379d9c49d05cb52f2
yujie <407278556@qq.com>
2023-09-22 18:51:01 +0800

    fix: restore server

---
+++ src/views/Test.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import Distribution from '/@/components/Distribution/index.vue';
+const lists = [
+	{
+		id: '898430',
+	},
+	{
+		id: 'iqwoipo',
+	},
+];
+</script>
+
+<template>
+	<Distribution force-open :lists="lists" />
+</template>
+
+<style scoped lang="scss"></style>
