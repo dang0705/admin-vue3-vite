5a5e1f14d5773f54e7bb0e0a954617856f6bca11
鲁秋 <17683835593@163.com>
2023-09-27 15:19:35 +0800

    feat: 承接记录

---
+++ src/api/core/undertakerTask.ts
@@ -0,0 +1,41 @@
+import request from "/@/utils/request"
+
+export function fetchList(query?: Object) {
+  return request({
+    url: '/core/undertakerTask/page',
+    method: 'get',
+    params: query
+  })
+}
+
+export function addObj(obj?: Object) {
+  return request({
+    url: '/core/undertakerTask',
+    method: 'post',
+    data: obj
+  })
+}
+
+export function getObj(id?: string) {
+  return request({
+    url: '/core/undertakerTask/' + id,
+    method: 'get'
+  })
+}
+
+export function delObjs(ids?: Object) {
+  return request({
+    url: '/core/undertakerTask',
+    method: 'delete',
+    data: ids
+  })
+}
+
+export function putObj(obj?: Object) {
+  return request({
+    url: '/core/undertakerTask',
+    method: 'put',
+    data: obj
+  })
+}
+
