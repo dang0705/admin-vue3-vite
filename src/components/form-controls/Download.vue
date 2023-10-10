1ccdcd49e3424119c24902d755a38fc9c550b416
yujie <407278556@qq.com>
2023-10-10 16:37:33 +0800

    feat: Add two components as table and download

---
+++ src/components/form-controls/Download.vue
@@ -0,0 +1,41 @@
+<script setup>
+import baseUrl from '/@/configuration/base-url';
+const DownloadComponent = {
+	props: {
+		modelValue: {
+			type: String,
+			required: true,
+		},
+		text: {
+			type: String,
+			required: true,
+		},
+	},
+	setup(props) {
+		const downloadLink = ref('');
+		onMounted(() => {
+			downloadLink.value = props.modelValue;
+		});
+
+		return {
+			downloadLink,
+		};
+	},
+	render() {
+		return h('div', [
+			h(
+				'a',
+				{
+					href: `${baseUrl}/${this.modelValue}`,
+					download: this.text,
+					class: 'text-primary',
+				},
+				this.text
+			),
+		]);
+	},
+};
+</script>
+<template>
+	<DownloadComponent />
+</template>
