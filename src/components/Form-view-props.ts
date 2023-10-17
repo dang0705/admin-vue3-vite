090fc72dab8a16e389c00dc440e4834ee3c42789
yujie <407278556@qq.com>
2023-10-17 14:42:47 +0800

    feat(README): Add FormView's API

--- src/components/Form-view-props.ts
+++ src/components/Form-view-props.ts
@@ -9,12 +9,11 @@ export interface FormOptions {
 	key: string; // 后端字段
 	props?: Props; // element ui 控件或自定义组件的props
 	optionUrl?: string; // 下拉/多选/单选组件的后端接口
-	options?: [] | object; // 下拉/多选/单选组件的子元素数组
+	options?: string | [] | object; // 字典/下拉/多选/单选组件的子元素数组，当控件为动态组件时，使用object类型
 	value?: unknown; // 组件默认数据
 	rules?: []; // 验证规则
 	slot?: boolean | string; //插槽,
 	title?: string; // 表单区间的标题
-	getValue?: Function; // 转义值的函数
 	hidden?: boolean; // 表单隐藏,但有表单数据
 }
 export default {
