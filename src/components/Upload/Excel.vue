a7fee3a25204511ea9646d24ffa537b01d1f6d90
Admin\WXL <wxl00cc@163.com>
2023-10-20 11:13:23 +0800

    前往批次后关闭弹框刷新页面

--- src/components/Upload/Excel.vue
+++ src/components/Upload/Excel.vue
@@ -269,7 +269,10 @@ const upload = async () => {
 	}
 };
 const router = useRouter();
-const goToBatchManagement = () => router.push({ name: '导入批次', state: { refresh: 1 } });
+const goToBatchManagement = () => {
+	router.push({ name: '导入批次', state: { refresh: 1 } });
+	handleCancel();
+};
 
 const handleCancel = () => {
 	state.successVisible = false;
