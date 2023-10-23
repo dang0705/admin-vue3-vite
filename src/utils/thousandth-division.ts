79d157faae715ee61c4319b7ca4c65d02e56eab1
yujie <407278556@qq.com>
2023-10-23 18:47:04 +0800

    feat(thousandth-division): Optimize number params

--- src/utils/thousandth-division.ts
+++ src/utils/thousandth-division.ts
@@ -5,9 +5,10 @@ import helper from '/@/utils/helpers';
  * @param number        {number|string}  源数字
  * @param decimals      {number}         小数位数
  */
-export default function ({ number, decimals = 2 }: Record<string, number | string>): string {
+export default function ({ number, decimals = 2 }: Record<string, number | string>): string | null {
 	number = helper.isNumber(number) ? number : helper.isString(number) ? +number : number;
 	if (number === 0) return '0.00';
+	if (number === null) return '--';
 
 	const str = (number as number).toFixed(decimals as number);
 	const parts = str.split('.');
