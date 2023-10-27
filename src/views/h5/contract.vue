770437e8bb86b27254685cf5eb07e62a9ca0560e
Admin\WXL <wxl00cc@163.com>
2023-10-27 13:22:40 +0800

    小程序h5页面

---
+++ src/views/h5/contract.vue
@@ -0,0 +1,16 @@
+<template>
+	<iframe
+		width="100%"
+		height="100%"
+		src="https://h5.sandbox.junziqian.com/signInfo/viewDetail?ts=1698375194703&signNo=SSN1717736182592851968&sign=IXgXKwWK0GnL1f1Jaj7HovEONX4Tl5uITVd9ze2f1lM"
+	></iframe>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+</style>
