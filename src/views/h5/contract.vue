12f3df59f207d39b64f4e90d51627611d68e001e
yujie <407278556@qq.com>
2023-10-31 14:22:26 +0800

    feat: Test h5 back 2 mp

--- src/views/h5/contract.vue
+++ src/views/h5/contract.vue
@@ -3,19 +3,19 @@
 		width="100%"
 		height="100%"
 		src="https://h5.sandbox.junziqian.com/signInfo/viewDetail?ts=1698375194703&signNo=SSN1717736182592851968&sign=IXgXKwWK0GnL1f1Jaj7HovEONX4Tl5uITVd9ze2f1lM"
-	></iframe>
+	/>
 </template>
 
 <script setup lang="ts">
-window.top.addEventListener(
-	'popstate',
-	function (e) {
-		history.go(-2);
-		console.log('我监听到了浏览器的返回按钮事件');
-	},
-	false
-);
+// window.top.addEventListener(
+// 	'popstate',
+// 	function (e) {
+// 		history.go(-2);
+// 		console.log('我监听到了浏览器的返回按钮事件');
+// 	},
+// 	false
+// );
+onMounted(() => wx.miniProgram.navigateBack());
 </script>
 
-<style>
-</style>
+<style></style>
