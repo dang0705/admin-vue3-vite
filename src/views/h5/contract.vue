eb46dc44e20dd6be8829a799bf4dc2a7f79201e6
yujie <407278556@qq.com>
2023-11-01 15:44:46 +0800

    feat: Mp navigateTo

--- src/views/h5/contract.vue
+++ src/views/h5/contract.vue
@@ -3,7 +3,7 @@
 		width="100%"
 		height="100%"
 		src="https://h5.sandbox.junziqian.com/signInfo/viewDetail?ts=1698375194703&signNo=SSN1717736182592851968&sign=IXgXKwWK0GnL1f1Jaj7HovEONX4Tl5uITVd9ze2f1lM"
-	/>
+	></iframe>
 </template>
 
 <script setup lang="ts">
@@ -15,7 +15,7 @@
 // 	},
 // 	false
 // );
-onMounted(() => wx.miniProgram.navigateBack());
+onMounted(() => wx.miniProgram.navigateTo({ url: 'pages/my/index' }));
 </script>
 
 <style></style>
