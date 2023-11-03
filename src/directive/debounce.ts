a8c9a45aec8d363540d6d764f91e918074cd3406
yujie <407278556@qq.com>
2023-11-03 14:48:18 +0800

    feat(debounce): Need updated hooks

--- src/directive/debounce.ts
+++ src/directive/debounce.ts
@@ -1,12 +1,18 @@
 import throttle from '/@/utils/throttle';
 import defaultDelay from '/@/configuration/form-view-action-debounce-delay';
 
+const debounce = ({ el, value, delay, params }: any) => {
+	const clickCallback = throttle(value, delay || defaultDelay);
+	el.onclick = !value ? console.error('v-debounce should give your click callback') : clickCallback.bind(this, params);
+};
 export default {
 	name: 'debounce',
 	directive: {
 		mounted(el: any, { value, arg: { delay, params } = {} }: any) {
-			const clickCallback = throttle(value, delay || defaultDelay);
-			el.onclick = !value ? console.error('v-debounce should give your click callback') : clickCallback.bind(this, params);
+			debounce({ el, value, delay, params });
+		},
+		updated(el: any, { value, arg: { delay, params } = {} }: any) {
+			debounce({ el, value, delay, params });
 		},
 	},
 };
