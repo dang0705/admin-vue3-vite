025ab7ac14298b48662417ba7063905e54a52e07
yujie <407278556@qq.com>
2023-11-14 16:03:10 +0800

    feat(home): Message

--- src/components/Upload/Excel.vue
+++ src/components/Upload/Excel.vue
@@ -1,303 +1,339 @@
 <!-- excel 导入组件 -->
 <template>
-	<div>
-		<el-dialog :title="title" v-model="state.upload.open" :close-on-click-modal="false" draggable>
-			<div class="guidance mb10">
-				<p v-html="guidance" />
-			</div>
-			<div v-if="tempUrl" class="my-6">
-				<a v-if="templateOnFront" class="color-primary hover:" download :href="tempUrl" v-text="$t('excel.downloadTemplate')" />
-				<el-link v-else type="primary" :underline="false" style="font-size: 12px; vertical-align: baseline" @click="downExcelTemp">
-					{{ $t('excel.downloadTemplate') }}
-				</el-link>
-			</div>
-			<el-divider />
-			<Form-view
-				:forms="forms"
-				:columns="24"
-				vertical
-				v-model="formData"
-				v-model:valid="valid"
-				v-model:show="state.upload.open"
-				:label-width="labelWidth"
-				:on-submit="upload"
-				:submit-button-text="submitButtonText"
-			>
-				<template v-for="(_, slot) in $slots" #[slot]>
-					<slot :name="slot" v-bind="{ formData }" />
-				</template>
-				<template #after-forms>
-					<el-form-item style="margin-bottom: 20px !important" :prop="mainField" :label="`${title}：`" :rules="excelRules">
-						<el-upload
-							action="#"
-							drag
-							ref="uploadRef"
-							:id="uuid"
-							:accept="accept.join(',')"
-							:auto-upload="false"
-							:limit="1"
-							:show-file-list="false"
-							:headers="headers"
-							:disabled="state.upload.isUploading"
-							:on-progress="handleFileUploadProgress"
-							:on-success="handleFileSuccess"
-							:on-error="handleFileError"
-							:on-change="onChange"
-						>
-							<i class="el-icon-upload" />
-							<div class="el-upload__text">
-								{{ $t('excel.operationNotice') }}
-								<em>{{ $t('excel.clickUpload') }}</em>
-							</div>
-							<template #tip>
-								<div class="el-upload__tip text-center">
-									<span>{{ $t('excel.fileFormat') }}</span>
-								</div>
-								<span v-if="fileName" class="text-primary" v-text="`文件名称：${fileName}`" />
-							</template>
-						</el-upload>
-					</el-form-item>
-				</template>
-			</Form-view>
-		</el-dialog>
+  <div>
+    <el-dialog
+      :title="title"
+      v-model="state.upload.open"
+      :close-on-click-modal="false"
+      draggable>
+      <div class="guidance mb10">
+        <p v-html="guidance" />
+      </div>
+      <div v-if="tempUrl" class="my-6">
+        <a
+          v-if="templateOnFront"
+          class="color-primary hover:"
+          download
+          :href="tempUrl"
+          v-text="$t('excel.downloadTemplate')" />
+        <el-link
+          v-else
+          type="primary"
+          :underline="false"
+          style="font-size: 12px; vertical-align: baseline"
+          @click="downExcelTemp">
+          {{ $t('excel.downloadTemplate') }}
+        </el-link>
+      </div>
+      <el-divider />
+      <Form-view
+        :forms="forms"
+        :columns="24"
+        vertical
+        v-model="formData"
+        v-model:valid="valid"
+        v-model:show="state.upload.open"
+        :label-width="labelWidth"
+        :on-submit="upload"
+        :submit-button-text="submitButtonText">
+        <template v-for="(_, slot) in $slots" #[slot]>
+          <slot :name="slot" v-bind="{ formData }" />
+        </template>
+        <template #after-forms>
+          <el-form-item
+            style="margin-bottom: 20px !important"
+            :prop="mainField"
+            :label="`${title}：`"
+            :rules="excelRules">
+            <el-upload
+              action="#"
+              drag
+              ref="uploadRef"
+              :id="uuid"
+              :accept="accept.join(',')"
+              :auto-upload="false"
+              :limit="1"
+              :show-file-list="false"
+              :headers="headers"
+              :disabled="state.upload.isUploading"
+              :on-progress="handleFileUploadProgress"
+              :on-success="handleFileSuccess"
+              :on-error="handleFileError"
+              :on-change="onChange">
+              <i class="el-icon-upload" />
+              <div class="el-upload__text">
+                {{ $t('excel.operationNotice') }}
+                <em>{{ $t('excel.clickUpload') }}</em>
+              </div>
+              <template #tip>
+                <div class="el-upload__tip text-center">
+                  <span>{{ $t('excel.fileFormat') }}</span>
+                </div>
+                <span
+                  v-if="fileName"
+                  class="text-primary"
+                  v-text="`文件名称：${fileName}`" />
+              </template>
+            </el-upload>
+          </el-form-item>
+        </template>
+      </Form-view>
+    </el-dialog>
 
-		<!--校验失败错误数据-->
-		<el-dialog :title="title" v-model="state.successVisible" @close="handleClose" :close-on-click-modal="false" v-if="toBatch">
-			<p v-text="state.upload.data" />
-			<template #footer>
-				<el-button type="primary" @click="goToBatchManagement">{{ $t('common.goToBatchManagement') }}</el-button>
-				<el-button @click="handleCancel">{{ $t('common.cancelButtonText') }}</el-button>
-			</template>
-		</el-dialog>
-	</div>
+    <!--校验失败错误数据-->
+    <el-dialog
+      :title="title"
+      v-model="state.successVisible"
+      @close="handleClose"
+      :close-on-click-modal="false"
+      v-if="toBatch">
+      <p v-text="state.upload.data" />
+      <template #footer>
+        <el-button type="primary" @click="goToBatchManagement">
+          {{ $t('common.goToBatchManagement') }}
+        </el-button>
+        <el-button @click="handleCancel">
+          {{ $t('common.cancelButtonText') }}
+        </el-button>
+      </template>
+    </el-dialog>
+  </div>
 </template>
 
 <script setup lang="ts">
-import { useMessage } from '/@/hooks/message';
-import other, { generateUUID } from '/@/utils/other';
-import { Session } from '/@/utils/storage';
-import request from '/@/utils/request';
-import { ElNotification } from 'element-plus';
-import { LIMIT } from '/@/configuration/upload-rules';
-defineOptions({ name: 'Upload-excel' });
-const uuid = ref('id-' + generateUUID());
+import { useMessage } from '/@/hooks/message'
+import other, { generateUUID } from '/@/utils/other'
+import { Session } from '/@/utils/storage'
+import request from '/@/utils/request'
+import { ElNotification } from 'element-plus'
+import { LIMIT } from '/@/configuration/upload-rules'
+defineOptions({ name: 'Upload-excel' })
+const uuid = ref('id-' + generateUUID())
 const prop = defineProps({
-	url: {
-		type: String,
-	},
-	title: {
-		type: String,
-	},
-	uploadUrl: {
-		type: String,
-		default: '/docs/sys-file/upload',
-	},
-	tempUrl: {
-		type: String,
-	},
-	forceOpen: {
-		type: Boolean,
-		default: false,
-	},
-	templateOnFront: {
-		type: Boolean,
-		default: false,
-	},
-	forms: {
-		type: Array,
-		default: () => [],
-	},
-	params: {
-		type: Object,
-		default: () => {},
-	},
-	fileSize: {
-		type: [Number, String],
-		default: LIMIT.excel,
-	},
-	formLabelWidth: {
-		type: [String, Number],
-		default: 140,
-	},
-	mainField: {
-		type: String,
-		default: 'file',
-	},
-	mainLabel: {
-		type: String,
-		default: '',
-	},
-	props: {
-		type: Object,
-		default: () => ({ label: 'label', value: 'value' }),
-	},
-	required: {
-		type: Boolean,
-		default: true,
-	},
-	rules: {
-		type: Array,
-		default: () => [],
-	},
-	/**
-	 * 指导文案, 处于对话框第一行
-	 */
-	guidance: {
-		type: String,
-		default: '',
-	},
-	successfulDialog: {
-		type: Boolean,
-		default: true,
-	},
-	inlineForm: {
-		type: Boolean,
-		default: false,
-	},
-	submitButtonText: {
-		type: String,
-		default: '确认导入',
-	},
-	params: {
-		type: Object,
-		default: () => ({}),
-	},
-	toBatch: {
-		type: Boolean,
-		default: true,
-	},
-	labelWidth: {
-		type: [String, Number],
-		default: 140,
-	},
-});
-const valid = ref(false);
-const uploadRef = ref();
-const emit = defineEmits(['refreshDataList']);
+  url: {
+    type: String
+  },
+  title: {
+    type: String
+  },
+  uploadUrl: {
+    type: String,
+    default: '/docs/sys-file/upload'
+  },
+  tempUrl: {
+    type: String
+  },
+  forceOpen: {
+    type: Boolean,
+    default: false
+  },
+  templateOnFront: {
+    type: Boolean,
+    default: false
+  },
+  forms: {
+    type: Array,
+    default: () => []
+  },
+  params: {
+    type: Object,
+    default: () => {}
+  },
+  fileSize: {
+    type: [Number, String],
+    default: LIMIT.excel
+  },
+  formLabelWidth: {
+    type: [String, Number],
+    default: 140
+  },
+  mainField: {
+    type: String,
+    default: 'file'
+  },
+  mainLabel: {
+    type: String,
+    default: ''
+  },
+  props: {
+    type: Object,
+    default: () => ({ label: 'label', value: 'value' })
+  },
+  required: {
+    type: Boolean,
+    default: true
+  },
+  rules: {
+    type: Array,
+    default: () => []
+  },
+  /**
+   * 指导文案, 处于对话框第一行
+   */
+  guidance: {
+    type: String,
+    default: ''
+  },
+  successfulDialog: {
+    type: Boolean,
+    default: true
+  },
+  inlineForm: {
+    type: Boolean,
+    default: false
+  },
+  submitButtonText: {
+    type: String,
+    default: '确认导入'
+  },
+  params: {
+    type: Object,
+    default: () => ({})
+  },
+  toBatch: {
+    type: Boolean,
+    default: true
+  },
+  labelWidth: {
+    type: [String, Number],
+    default: 140
+  }
+})
+const valid = ref(false)
+const uploadRef = ref()
+const emit = defineEmits(['refreshDataList'])
 
 const state = reactive({
-	successVisible: false,
-	errorData: [],
-	dialog: {
-		title: '',
-	},
-	upload: {
-		open: false,
-		isUploading: false,
-		data: '',
-	},
-});
-const accept = ['.xlsx', '.xls'];
-const formData = ref({ ...prop.params });
+  successVisible: false,
+  errorData: [],
+  dialog: {
+    title: ''
+  },
+  upload: {
+    open: false,
+    isUploading: false,
+    data: ''
+  }
+})
+const accept = ['.xlsx', '.xls']
+const formData = ref({ ...prop.params })
 const excelRules = ref([
-	{
-		required: prop.required,
-		...(prop.required ? { trigger: 'blur', message: `${prop.mainLabel}必须上传` } : {}),
-	},
-]);
+  {
+    required: prop.required,
+    ...(prop.required
+      ? { trigger: 'blur', message: `${prop.mainLabel}必须上传` }
+      : {})
+  }
+])
 // 控制表单控件的对象结构
-const open = computed(() => prop.forceOpen || state.upload.open);
+const open = computed(() => prop.forceOpen || state.upload.open)
 /**
  * 下载模板文件
  */
 const downExcelTemp = () => {
-	other.downBlobFile(prop.templateOnFront ? prop.tempUrl : other.adaptationUrl(prop.tempUrl), {}, 'temp.xlsx');
-};
+  other.downBlobFile(
+    prop.templateOnFront ? prop.tempUrl : other.adaptationUrl(prop.tempUrl),
+    {},
+    'temp.xlsx'
+  )
+}
 
 /**
  * 上传进度条变化事件
  */
 const handleFileUploadProgress = () => {
-	state.upload.isUploading = true;
-};
-let fileName = ref('');
+  state.upload.isUploading = true
+}
+let fileName = ref('')
 
 const onChange = (e) => {
-	const excelSize = e.size / 1024 / 1024 < prop.fileSize;
-	const excelType = accept.includes(e.name.slice(e.name.lastIndexOf('.')));
-	if (!excelSize || !excelType) {
-		setTimeout(() => {
-			ElNotification({
-				title: '温馨提示',
-				message: !excelSize ? `上传图片大小不能超过 ${prop.fileSize}M！` : `文件必须是${accept.join('和')}后缀`,
-				type: 'warning',
-			});
-		}, 0);
-		formData.value[prop.mainField] = null;
-		fileName.value = null;
-		uploadRef.value.clearFiles();
-		return;
-	}
-	/*	ElNotification({
+  const excelSize = e.size / 1024 / 1024 < prop.fileSize
+  const excelType = accept.includes(e.name.slice(e.name.lastIndexOf('.')))
+  if (!excelSize || !excelType) {
+    setTimeout(() => {
+      ElNotification({
+        title: '温馨提示',
+        message: !excelSize
+          ? `上传图片大小不能超过 ${prop.fileSize}M！`
+          : `文件必须是${accept.join('和')}后缀`,
+        type: 'warning'
+      })
+    }, 0)
+    formData.value[prop.mainField] = null
+    fileName.value = null
+    uploadRef.value.clearFiles()
+    return
+  }
+  /*	ElNotification({
 		title: '温馨提示',
 		message: '文件上传成功！',
 		type: 'success',
 	});*/
-	formData.value[prop.mainField] = e.raw;
-	fileName.value = e.name;
-	uploadRef.value.clearFiles();
-};
+  formData.value[prop.mainField] = e.raw
+  fileName.value = e.name
+  uploadRef.value.clearFiles()
+}
 const upload = async () => {
-	let formDataObject = new FormData();
-	for (let key in formData.value) {
-		formDataObject.append(key, formData.value[key]);
-	}
-	for (let key in prop.params) {
-		formDataObject.append(key, prop.params[key]);
-	}
+  let formDataObject = new FormData()
+  for (let key in formData.value) {
+    formDataObject.append(key, formData.value[key])
+  }
+  for (let key in prop.params) {
+    formDataObject.append(key, prop.params[key])
+  }
 
-	try {
-		const { data } = await request({
-			url: prop.uploadUrl,
-			method: 'post',
-			headers: {
-				'Content-Type': 'multipart/form-data',
-				...headers.value,
-			},
-			data: formDataObject,
-		});
-		state.upload.data = data;
-		if (prop.successfulDialog) {
-			state.successVisible = true;
-		}
-		state.upload.open = false;
-	} catch (error) {
-		console.log(error);
-		// onError(error as any);
-	}
-};
-const router = useRouter();
+  try {
+    const { data } = await request({
+      url: prop.uploadUrl,
+      method: 'post',
+      headers: {
+        'Content-Type': 'multipart/form-data',
+        ...headers.value
+      },
+      data: formDataObject
+    })
+    state.upload.data = data
+    if (prop.successfulDialog) {
+      state.successVisible = true
+    }
+    state.upload.open = false
+  } catch (error) {
+    console.log(error)
+    // onError(error as any);
+  }
+}
+const router = useRouter()
 const goToBatchManagement = () => {
-	router.push({ name: '导入批次', state: { refresh: 1 } });
-	handleCancel();
-};
+  router.push({ name: '导入批次', state: { refresh: 1 } })
+  handleCancel()
+}
 
 const handleCancel = () => {
-	state.successVisible = false;
-};
+  state.successVisible = false
+}
 
 const handleClose = () => {
-	emit?.('refreshDataList');
-};
+  emit?.('refreshDataList')
+}
 /**
  * 上传失败事件处理
  */
 const handleFileError = () => {
-	useMessage().error('上传失败,数据格式不合法!');
-	state.upload.open = false;
-};
+  useMessage().error('上传失败,数据格式不合法!')
+  state.upload.open = false
+}
 
 /**
  * 上传成功事件处理
  * @param {any} response - 上传成功的响应结果
  */
 const handleFileSuccess = (response: any) => {
-	ElNotification({
-		title: '温馨提示',
-		message: '图片上传成功！',
-		type: 'success',
-	});
-	/*	state.upload.isUploading = false;
+  ElNotification({
+    title: '温馨提示',
+    message: '图片上传成功！',
+    type: 'success'
+  })
+  /*	state.upload.isUploading = false;
 	state.upload.open = false;
 	uploadRef.value.clearFiles();
 
@@ -314,38 +350,41 @@ const handleFileSuccess = (response: any) => {
 		// 刷新表格
 		emit?.('refreshDataList');
 	}*/
-};
+}
 
 /**
  * 显示上传文件对话框，并清除上传信息
  */
 const openDialog = async () => {
-	state.upload.isUploading = false;
-	state.upload.open = true;
-	fileName.value = '';
-	// initForms();
-};
+  state.upload.isUploading = false
+  state.upload.open = true
+  fileName.value = ''
+  // initForms();
+}
 
 /**
  * 计算请求头部信息
  */
 const headers = computed(() => {
-	return {
-		Authorization: 'Bearer ' + Session.getToken(),
-		'TENANT-ID': Session.getTenant(),
-	};
-});
-const handleDragStart = () => document.activeElement?.blur();
-window.addEventListener('dragenter', handleDragStart, true);
-onUnmounted(() => window.removeEventListener('dragstart', handleDragStart));
+  return {
+    Authorization: 'Bearer ' + Session.getToken(),
+    'TENANT-ID': Session.getTenant()
+  }
+})
+const handleDragStart = async (e) => {
+  await nextTick()
+  document.activeElement?.blur()
+}
+window.addEventListener('dragenter', handleDragStart, true)
+onUnmounted(() => window.removeEventListener('dragstart', handleDragStart))
 // 暴露变量
 defineExpose({
-	openDialog,
-});
+  openDialog
+})
 </script>
 
 <style scoped>
 .el-upload__tip {
-	text-align: left;
+  text-align: left;
 }
 </style>
--- src/views/message/index.vue
+++ src/views/message/index.vue
@@ -1,101 +1,108 @@
 <script setup lang="ts">
-import { deleteObj, readMark } from '/@/api/docs/message';
-const $router = useRouter();
-const selectObjs = ref([]); // 勾选的表格行
+import { deleteObj, readMark } from '/@/api/docs/message'
+import bgc from '/src/configuration/message-tag-definition'
+const $router = useRouter()
+const selectObjs = ref([]) // 勾选的表格行
 const columns = [
-	{
-		type: 'selection',
-		prop: 'delete',
-		width: 40,
-	},
-	{
-		prop: 'title',
-		headerSlot: true,
-		slot: true,
-		width: 100,
-	},
-	{
-		prop: 'content',
-		slot: true,
-		align: 'left',
-	},
-	{
-		prop: 'right',
-		headerSlot: true,
-		slot: true,
-		width: 200,
-	},
-];
-
-const bgc = (noticeType: string) => {
-	switch (noticeType) {
-		case '1':
-			return 'background: rgba(83, 195, 26, .15);color: #53C31A';
-		case '2':
-			return 'background: #FFE1E1;color: #FF0000';
-		case '3':
-			return 'background: #DBE9FF;color: #0065FF';
-		default:
-			return 'background: rgba(83, 195, 26, .15);color: #53C31A';
-	}
-};
+  {
+    type: 'selection',
+    prop: 'delete',
+    width: 40
+  },
+  {
+    prop: 'title',
+    headerSlot: true,
+    slot: true,
+    width: 100
+  },
+  {
+    prop: 'content',
+    slot: true,
+    align: 'left'
+  },
+  {
+    prop: 'right',
+    headerSlot: true,
+    slot: true,
+    width: 200
+  }
+]
 
 const delMessage = async (refresh: any) => {
-	await deleteObj(selectObjs.value);
-	refresh();
-};
+  await deleteObj(selectObjs.value)
+  refresh()
+}
 
 const goDetail = async (id: string, url: string, refresh: any) => {
-	console.log(id, url, 123123);
-	$router.push({ path: url });
-	await readMark([id]);
-	refresh();
-};
+  console.log(id, url, 123123)
+  $router.push({ path: url })
+  await readMark([id])
+  refresh()
+}
 
 const readMarkAll = async (refresh: any) => {
-	await readMark([]);
-	refresh();
-};
+  await readMark([])
+  refresh()
+}
 </script>
 
 <template>
-	<Table-view module="docs/message.ts" :columns="columns" noFormView no-header :border="false" v-model="selectObjs">
-		<template #top-bar>
-			<div class="text-[18px] font-bold">事项提醒</div>
-			<!-- <el-divider /> -->
-		</template>
-		<template #title-header="{ refresh }">
-			<el-button text @click="delMessage(refresh)" :disabled="!selectObjs.length">删除</el-button>
-		</template>
-		<template #title="{ row }">
-			<div class="rounded-[3px] relative" :style="bgc(row.noticeType)">
-				{{ row.title }}
-				<div v-if="row.readStatus === '0'" class="w-[6px] h-[6px] rounded-[50%] bg-[#FF6826] absolute top-[-3px] left-0"></div>
-			</div>
-		</template>
-		<template #content="{ row }">
-			<div>{{ row.content }}</div>
-			<div class="text-[12px] text-[#999999]">{{ row.createTime }}</div>
-		</template>
-		<template #right-header="{ refresh }">
-			<el-button type="primary" @click="readMarkAll(refresh)">标记全部已读</el-button>
-		</template>
-		<template #right="{ row, refresh }">
-			<div class="text-[#0065FF] cursor-pointer" @click="goDetail(row.id, row.url, refresh)">详情 &nbsp; &gt;</div>
-			<!-- <router-link v-if="row.url" :to="{ path: row.url }" class="flex-shrink-0 text-xs text-[#0065FF]">详情 &nbsp; &gt;</router-link> -->
-		</template>
-	</Table-view>
+  <Table-view
+    module="docs/message.ts"
+    :columns="columns"
+    noFormView
+    no-header
+    :border="false"
+    v-model="selectObjs">
+    <template #top-bar>
+      <div class="text-[18px] font-bold">事项提醒</div>
+      <!-- <el-divider /> -->
+    </template>
+    <template #title-header="{ refresh }">
+      <el-button
+        text
+        @click="delMessage(refresh)"
+        :disabled="!selectObjs.length">
+        删除
+      </el-button>
+    </template>
+    <template #title="{ row }">
+      <div class="rounded-[3px] relative" :style="bgc(row.noticeType)">
+        {{ row.title }}
+        <div
+          v-if="row.readStatus === '0'"
+          class="w-[6px] h-[6px] rounded-[50%] bg-[#FF6826] absolute top-[-3px] left-0"></div>
+      </div>
+    </template>
+    <template #content="{ row }">
+      <div>{{ row.content }}</div>
+      <div class="text-[12px] text-[#999999]">{{ row.createTime }}</div>
+    </template>
+    <template #right-header="{ refresh }">
+      <el-button type="primary" @click="readMarkAll(refresh)">
+        标记全部已读
+      </el-button>
+    </template>
+    <template #right="{ row, refresh }">
+      <div
+        class="text-[#0065FF] cursor-pointer"
+        @click="goDetail(row.id, row.url, refresh)">
+        详情 &nbsp; &gt;
+      </div>
+      <!-- <router-link v-if="row.url" :to="{ path: row.url }" class="flex-shrink-0 text-xs text-[#0065FF]">详情 &nbsp; &gt;</router-link> -->
+    </template>
+  </Table-view>
 </template>
 <script lang="ts">
 export default {
-	created() {
-		this.$options.name = this.$route.meta.title;
-	},
-};
+  created() {
+    this.$options.name = this.$route.meta.title
+  }
+}
 </script>
 
 <style scoped lang="scss">
 :deep(.el-tooltip) {
-	overflow: visible !important;
+  overflow: visible !important;
 }
 </style>
