92af2488925982a9d08168918ac9b6ccc259e453
yujie <407278556@qq.com>
2023-11-15 09:56:48 +0800

    style: Format code style

--- src/hooks/keep-alive-list-refresh.ts
+++ src/hooks/keep-alive-list-refresh.ts
@@ -3,7 +3,7 @@
  * @param resetQuery     刷列表方法
  */
 export default (resetQuery: any) =>
-	onActivated(async () => {
-		history.state.refresh && resetQuery();
-		history.state.refresh = null;
-	});
+  onActivated(async () => {
+    history.state.refresh && resetQuery()
+    history.state.refresh = null
+  })
