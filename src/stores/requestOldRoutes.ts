2977b1be1043d646349f03b9398ef3feba115287
于杰 <407278556@qq.com>
2023-11-17 23:20:30 +0800

    fix(pinia): Plugin colada's produtcion error

--- src/stores/requestOldRoutes.ts
+++ src/stores/requestOldRoutes.ts
@@ -1,26 +1,28 @@
-import { defineStore } from 'pinia';
 interface Route {
-	children: Object[];
-	[x: string]: any;
+  children: Object[]
+  [x: string]: any
 }
 /**
  * 后端返回原始路由(未处理时)
  * @methods setCacheKeepAlive 设置接口原始路由数据
  */
 export const useRequestOldRoutes = defineStore('requestOldRoutes', {
-	state: (): { requestOldRoutes: Route[] } => ({
-		requestOldRoutes: [],
-	}),
-	getters: {
-		modules() {
-			let modules: any[] = [];
-			this.requestOldRoutes.forEach(({ children }) => modules.push(...children));
-			return modules.map(({ meta: { enName: module }, name }) => ({ module, name }));
-		},
-	},
-	actions: {
-		async setRequestOldRoutes(routes: Route[]) {
-			this.requestOldRoutes = routes;
-		},
-	},
-});
+  state: (): { requestOldRoutes: Route[] } => ({
+    requestOldRoutes: []
+  }),
+  getters: {
+    modules() {
+      let modules: any[] = []
+      this.requestOldRoutes.forEach(({ children }) => modules.push(...children))
+      return modules.map(({ meta: { enName: module }, name }) => ({
+        module,
+        name
+      }))
+    }
+  },
+  actions: {
+    async setRequestOldRoutes(routes: Route[]) {
+      this.requestOldRoutes = routes
+    }
+  }
+})
--- src/stores/routesList.ts
+++ src/stores/routesList.ts
@@ -1,5 +1,3 @@
-import { defineStore } from 'pinia';
-
 /**
  * 路由列表
  * @methods setRoutesList 设置路由数据
@@ -7,20 +5,20 @@ import { defineStore } from 'pinia';
  * @methods setColumnsNavHover 设置分栏布局最左侧导航鼠标移入 boolean
  */
 export const useRoutesList = defineStore('routesList', {
-	state: (): RoutesListState => ({
-		routesList: [],
-		isColumnsMenuHover: false,
-		isColumnsNavHover: false,
-	}),
-	actions: {
-		async setRoutesList(data: Array<string>) {
-			this.routesList = data;
-		},
-		async setColumnsMenuHover(bool: Boolean) {
-			this.isColumnsMenuHover = bool;
-		},
-		async setColumnsNavHover(bool: Boolean) {
-			this.isColumnsNavHover = bool;
-		},
-	},
-});
+  state: (): RoutesListState => ({
+    routesList: [],
+    isColumnsMenuHover: false,
+    isColumnsNavHover: false
+  }),
+  actions: {
+    async setRoutesList(data: Array<string>) {
+      this.routesList = data
+    },
+    async setColumnsMenuHover(bool: Boolean) {
+      this.isColumnsMenuHover = bool
+    },
+    async setColumnsNavHover(bool: Boolean) {
+      this.isColumnsNavHover = bool
+    }
+  }
+})
