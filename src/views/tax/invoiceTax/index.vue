8f10955e3a6bccc7749c37e3956be1ba56136113
鲁秋 <17683835593@163.com>
2023-11-17 17:03:57 +0800

    fix: 代码格式化

--- src/views/tax/invoiceTax/index.vue
+++ src/views/tax/invoiceTax/index.vue
@@ -1,90 +1,97 @@
 <template>
-  <Table-view :staticQuery="staticQuery" :columns="columns" getListFnName="taxMerchantInvoice" :condition-forms="conditionForms" module="tax/index.ts">
+  <Table-view
+    :staticQuery="staticQuery"
+    :columns="columns"
+    getListFnName="taxMerchantInvoice"
+    :condition-forms="conditionForms"
+    module="tax/index.ts">
     <template #actions="{ row }">
-      <el-button v-auth="'taxSupervision_merchantInvoice_down'" icon="download" text type="primary" @click="handleContractFile(row)"> 下载凭证 </el-button>
+      <el-button
+        v-auth="'taxSupervision_merchantInvoice_down'"
+        icon="download"
+        text
+        type="primary"
+        @click="handleContractFile(row)">
+        下载凭证
+      </el-button>
     </template>
   </Table-view>
 </template>
 
 <script setup lang="ts">
-import { useMessage, useMessageBox } from '/@/hooks/message';
-import { payChannel } from '/@/configuration/dynamic-control';
-const { proxy } = getCurrentInstance();
-const route: any = useRoute();
-const router = useRouter();
-const form = reactive({});
-
+const { proxy } = getCurrentInstance()
+const route: any = useRoute()
 const columns = [
-	{
-		prop: 'spName',
-		label: '服务商',
-		'min-width': 150,
-	},
-	{
-		prop: 'merchantName',
-		label: '商户',
-		'min-width': 150,
-	},
-	{
-		prop: 'invoicingCategoriesDesc',
-		label: '发票类目',
-		'min-width': 180,
-	},
-	{
-		prop: 'invoicingAmount',
-		label: '发票金额(元)',
-		'min-width': 180,
-	},
-	{
-		prop: 'updateTime',
-		label: '开票时间',
-		'min-width': 180,
-	},
-	{
-		label: '操作',
-		prop: 'actions',
-		fixed: 'right',
-		slot: true,
-		'min-width': 120,
-	},
-];
+  {
+    prop: 'spName',
+    label: '服务商',
+    'min-width': 150
+  },
+  {
+    prop: 'merchantName',
+    label: '商户',
+    'min-width': 150
+  },
+  {
+    prop: 'invoicingCategoriesDesc',
+    label: '发票类目',
+    'min-width': 180
+  },
+  {
+    prop: 'invoicingAmount',
+    label: '发票金额(元)',
+    'min-width': 180
+  },
+  {
+    prop: 'updateTime',
+    label: '开票时间',
+    'min-width': 180
+  },
+  {
+    label: '操作',
+    prop: 'actions',
+    fixed: 'right',
+    slot: true,
+    'min-width': 120
+  }
+]
 const staticQuery = computed(() => {
-	return {
-		spId: route.query.spId,
-		merchantId: route.query.merchantId,
-	};
-});
+  return {
+    spId: route.query.spId,
+    merchantId: route.query.merchantId
+  }
+})
 const conditionForms = [
-	{
-		control: 'SpSelect',
-		key: 'spId',
-		label: '服务商',
-		props: { platform: true },
-		value: route.query.spId,
-	},
-	{
-		control: 'MerchantSelect',
-		key: 'merchantId',
-		label: '商户',
-		value: route.query.merchantId,
-	},
-	{
-		control: 'DateRange',
-		key: 'queryTimeRange',
-		label: '开票时间',
-		props: {
-			valueType: 'string',
-		},
-	},
-];
+  {
+    control: 'SpSelect',
+    key: 'spId',
+    label: '服务商',
+    props: { platform: true },
+    value: route.query.spId
+  },
+  {
+    control: 'MerchantSelect',
+    key: 'merchantId',
+    label: '商户',
+    value: route.query.merchantId
+  },
+  {
+    control: 'DateRange',
+    key: 'queryTimeRange',
+    label: '开票时间',
+    props: {
+      valueType: 'string'
+    }
+  }
+]
 const handleContractFile = (row: any) => {
-	window.open(`${proxy.baseURL}/${row.invoiceFilePath}`);
-};
+  window.open(`${proxy.baseURL}/${row.invoiceFilePath}`)
+}
 </script>
 <script lang="ts">
 export default {
-	created() {
-		this.$options.name = this.$route.meta.title;
-	},
-};
+  created() {
+    this.$options.name = this.$route.meta.title
+  }
+}
 </script>
