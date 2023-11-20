4511e9d64efdd6907ca6407913298f58ffc47beb
Admin\WXL <wxl00cc@163.com>
2023-11-20 14:27:13 +0800

    首页站内信增加站内信跳转 站内信内页增加全部已读全部删除

--- src/views/message/index.vue
+++ src/views/message/index.vue
@@ -1,9 +1,7 @@
 <script setup lang="ts">
-import { deleteObj, readMark } from '/@/api/docs/message'
+import { deleteObj, readMark, readUnread } from '/@/api/docs/message'
 import bgc from '/@/configuration/message-tag-definition'
-let timeShow = ref(true)
 const $router = useRouter()
-const selectObjs = ref([]) // 勾选的表格行
 const columns = [
   {
     prop: 'title',
@@ -24,13 +22,22 @@ const columns = [
   }
 ]
 
-const delMessage = async (refresh: any) => {
-  await deleteObj(selectObjs.value)
+const delMessageAll = async (refresh: any) => {
+  await deleteObj([])
+  refresh()
+}
+
+const delMessage = async (id: string, refresh: any) => {
+  await deleteObj([id])
+  refresh()
+}
+
+const readMarkOne = async (id: string, refresh: any) => {
+  await readMark([id])
   refresh()
 }
 
 const goDetail = async (id: string, url: string, refresh: any) => {
-  console.log(id, url, 123123)
   $router.push({ path: url })
   await readMark([id])
   refresh()
@@ -38,6 +45,11 @@ const goDetail = async (id: string, url: string, refresh: any) => {
 
 const readMarkAll = async (refresh: any) => {
   await readMark([])
+  refresh()
+}
+
+const readMarkUnread = async (id: string, refresh: any) => {
+  await readUnread(id)
   refresh()
 }
 </script>
@@ -54,75 +66,82 @@ const readMarkAll = async (refresh: any) => {
       <!-- <el-divider /> -->
       <div class="w-[100%] h-[1px] bg-[#F3F3F3] mt-[10px] mb-[20px]"></div>
     </template>
-    <!-- <template #title-header="{ refresh }">
-      <el-button
-        text
-        @click="delMessage(refresh)"
-        :disabled="!selectObjs.length">
-        删除
+    <template #tab-right="{ refresh }">
+      <el-button @click="delMessageAll(refresh)">
+        <SvgIcon
+          name="iconfont icon-shanchu"
+          :size="10"
+          color="#858585"
+          class="mr-[5px]" />
+        删除全部
       </el-button>
-    </template> -->
+      <el-button type="primary" @click="readMarkAll(refresh)">
+        <SvgIcon
+          name="iconfont icon-biaojiweiyidu"
+          :size="10"
+          color="#ffffff"
+          class="mr-[5px]" />
+        全部已读
+      </el-button>
+    </template>
     <template #title="{ row }">
       <div
         class="rounded-[3px] relative flex justify-center items-center"
         :style="bgc(row.noticeType)">
         {{ row.title }}
-        <!-- <div
-          v-if="row.readStatus === '0'"
-          class="w-[6px] h-[6px] rounded-[50%] bg-[#FF6826] absolute top-[-3px] left-0"></div> -->
       </div>
     </template>
-    <template #content="{ row }">
-      <div>{{ row.content }}</div>
-    </template>
-    <!-- <template #right-header="{ refresh }">
-      <el-button type="primary" @click="readMarkAll(refresh)">
-        标记全部已读
-      </el-button>
-    </template> -->
-    <template #right="{ row, refresh }">
-      <!-- <div
-        class="text-[#0065FF] cursor-pointer"
-        @click="goDetail(row.id, row.url, refresh)">
-        详情 &nbsp; &gt;
-      </div> -->
+    <template #content="{ row, refresh }">
       <div
-        @mouseenter="timeShow = false"
-        v-if="timeShow"
-        class="text-[12px] text-[#999999] py-[10px]">
-        {{ row.createTime }}
+        :class="['cursor-pointer', { 'opacity-[0.5]': row.readStatus === '1' }]"
+        @click="goDetail(row.id, row.url, refresh)">
+        {{ row.content }}
       </div>
-      <div class="py-[10px]" v-if="!timeShow" @mouseleave="timeShow = true">
-        <el-tooltip
-          content="设为已读"
-          placement="top"
-          v-if="row.readStatus === '0'">
-          <SvgIcon
-            name="iconfont icon-biaojiweiyidu"
-            :size="13"
-            color="#858585"
-            class="mr-[18px] cursor-pointer"
-            @click="readMarkOne(row.id, refresh)" />
-        </el-tooltip>
-        <el-tooltip
-          content="设为未读"
-          placement="top"
-          v-if="row.readStatus === '1'">
-          <SvgIcon
-            name="iconfont icon-biaojiweiweidu"
-            :size="13"
-            color="#858585"
-            class="mr-[18px] cursor-pointer"
-            @click="readMarkUnread(row.id, refresh)" />
-        </el-tooltip>
-        <el-tooltip content="删除" placement="top">
-          <SvgIcon
-            name="iconfont icon-shanchu"
-            :size="13"
-            color="#858585"
-            class="cursor-pointer"
-            @click="delMessage(row.id, refresh)" />
-        </el-tooltip>
+    </template>
+    <template #right="{ row, refresh }">
+      <div
+        class="h-[52px] relative py-[10px] box-border"
+        @mouseleave.passive="row.show = true">
+        <div
+          @mouseenter="row.show = false"
+          v-show="row.show"
+          class="text-[12px] h-full text-[#999] cursor-pointer">
+          {{ row.createTime }}
+        </div>
+        <div class="absolute h-full w-full" v-show="!row.show">
+          <el-tooltip
+            content="设为已读"
+            :show-after="300"
+            placement="top"
+            v-if="row.readStatus === '0'">
+            <SvgIcon
+              name="iconfont icon-biaojiweiyidu"
+              :size="13"
+              color="#858585"
+              class="mr-[18px] cursor-pointer"
+              @click="readMarkOne(row.id, refresh)" />
+          </el-tooltip>
+          <el-tooltip
+            content="设为未读"
+            :show-after="300"
+            placement="top"
+            v-if="row.readStatus === '1'">
+            <SvgIcon
+              name="iconfont icon-biaojiweiweidu"
+              :size="13"
+              color="#858585"
+              class="mr-[18px] cursor-pointer"
+              @click="readMarkUnread(row.id, refresh)" />
+          </el-tooltip>
+          <el-tooltip :show-after="300" content="删除" placement="top">
+            <SvgIcon
+              name="iconfont icon-shanchu"
+              :size="13"
+              color="#858585"
+              class="cursor-pointer"
+              @click="delMessage(row.id, refresh)" />
+          </el-tooltip>
+        </div>
       </div>
     </template>
   </Table-view>
