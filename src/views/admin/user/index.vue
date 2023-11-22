46b950357318d5604b1726a78369731dd6f8ba31
Admin\WXL <wxl00cc@163.com>
2023-11-22 14:57:48 +0800

    用户管理解决白屏问题

--- src/views/admin/user/index.vue
+++ src/views/admin/user/index.vue
@@ -1,44 +1,46 @@
 <template>
-  <Table-view
-    :condition-forms="conditionForms"
-    :columns="columns"
-    :actions="actions"
-    label-width="100"
-    downBlobFileUrl="/admin/user/export"
-    downBlobFileName="users.xlsx"
-    exportAuth="sys_user_export"
-    module="admin/user">
-    <template #top-bar>
-      <el-button
-        v-auth="'sys_user_add'"
-        icon="folder-add"
-        type="primary"
-        @click="userDialogRef.openDialog()">
-        {{ $t('common.addBtn') }}
-      </el-button>
-    </template>
-    <template #roleName="{ row }">
-      <el-tag v-for="(item, index) in row.roleList" :key="index">
-        {{ item.roleName }}
-      </el-tag>
-    </template>
-  </Table-view>
+  <div>
+    <Table-view
+      :condition-forms="conditionForms"
+      :columns="columns"
+      :actions="actions"
+      label-width="100"
+      downBlobFileUrl="/admin/user/export"
+      downBlobFileName="users.xlsx"
+      exportAuth="sys_user_export"
+      module="admin/user">
+      <template #top-bar>
+        <el-button
+          v-auth="'sys_user_add'"
+          icon="folder-add"
+          type="primary"
+          @click="userDialogRef.openDialog()">
+          {{ $t('common.addBtn') }}
+        </el-button>
+      </template>
+      <template #roleName="{ row }">
+        <el-tag v-for="(item, index) in row.roleList" :key="index">
+          {{ item.roleName }}
+        </el-tag>
+      </template>
+    </Table-view>
 
-  <user-form ref="userDialogRef" @refresh="getDataList(false)" />
-  <Distribution
-    ref="customersRef"
-    id-filed="userId"
-    :titles="['未分配商户', '已分配商户']"
-    list-url="core/userMgrMerchant/assignMerchantPage"
-    save-url="core/userMgrMerchant/assignMerchant"
-    title="批量分配商户" />
-  <Distribution
-    ref="providerRef"
-    :titles="['未分配服务商', '已分配服务商']"
-    id-filed="userId"
-    list-url="core/userMgrSp/assignSpPage"
-    save-url="core/userMgrSp/assignSP"
-    title="批量分配服务商" />
+    <user-form ref="userDialogRef" @refresh="getDataList(false)" />
+    <Distribution
+      ref="customersRef"
+      id-filed="userId"
+      :titles="['未分配商户', '已分配商户']"
+      list-url="core/userMgrMerchant/assignMerchantPage"
+      save-url="core/userMgrMerchant/assignMerchant"
+      title="批量分配商户" />
+    <Distribution
+      ref="providerRef"
+      :titles="['未分配服务商', '已分配服务商']"
+      id-filed="userId"
+      list-url="core/userMgrSp/assignSpPage"
+      save-url="core/userMgrSp/assignSP"
+      title="批量分配服务商" />
+  </div>
 </template>
 
 <script lang="ts" setup>
