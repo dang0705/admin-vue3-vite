317b5e6335f9e9986eb3e469ddf295944bb4df65
yujie <407278556@qq.com>
2023-12-06 11:37:56 +0800

    chore(env): Optimize env file configurations

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,6 @@ const isPure = pureApps.includes($appName)
 
 // @ts-ignore
 const viteConfig = defineConfig(({ mode, command }: ConfigEnv) => {
-  
   const env = loadEnv(mode, process.cwd())
   const _isDev_ = process.argv.includes('dev')
   return {
