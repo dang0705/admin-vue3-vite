4efa74544b142805e5891dccc8ccf30d7116608a
鲁秋 <17683835593@163.com>
2023-12-13 16:14:42 +0800

    feat: iframe

--- apps/jmyg/src/views/bpm/form/configurations/condition-forms.ts
+++
@@ -1,7 +0,0 @@
-export default [
-  {
-    label: '表单名称',
-    key: 'name',
-    control: 'el-input'
-  }
-]
--- apps/jmyg/src/views/bpm/form/editor/index.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="container">12</div>
-</template>
-
-<script setup lang="ts"></script>
-
-<style scoped></style>
