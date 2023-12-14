61348fd1b6eecc270dc8ef592d4d2fa970e66e9d
yujie <407278556@qq.com>
2023-12-14 10:11:39 +0800

    fix(merchant/salaryPlan/configurations/trial-of-the-plan): Some changes

--- apps/jmyg/src/views/merchant/salaryPlan/configurations/trial-of-the-plan.tsx
+++ apps/jmyg/src/views/merchant/salaryPlan/configurations/trial-of-the-plan.tsx
@@ -2,7 +2,10 @@ import { useDict } from '@hooks/dict'
 import array2Object from '@utils/array-2-object'
 import { trialPlan } from '@jmyg/api/outsourcing/salaryPlan'
 
-let { salary_plan_project_type } = useDict('salary_plan_project_type')
+let { salary_plan_project_type, salary_plan_project_source } = useDict(
+  'salary_plan_project_type',
+  'salary_plan_project_source'
+)
 export default (
   dialog: any,
   list: any[],
@@ -13,24 +16,32 @@ export default (
   const controlTypeMap: Record<string, any> = array2Object({
     array: salary_plan_project_type.value as []
   })
+  const sourceTypeMap: Record<string, any> = array2Object({
+    array: salary_plan_project_source.value as []
+  })
   dialog.title = '方案试算'
   dialog.save = false
   dialog.keepShowAfterConfirm = true
-  dialog.forms = list.map(({ projectType, projectName }: any, index) => {
-    const control =
-      controlTypeMap[projectType] === '日期'
-        ? 'DateRange'
-        : controlTypeMap[projectType] === '数值'
-        ? 'el-input-number'
-        : 'el-input'
-    return {
-      ...(!index ? { title: '输入项' } : {}),
-      label: projectName,
-      key: projectName,
-      control,
-      ...(__isDev ? { value: 1 } : {})
+  const forms: any[] = []
+  list.forEach(({ projectType, projectName, projectSource }, index) => {
+    if (sourceTypeMap[projectSource] !== '计算项') {
+      const control =
+        controlTypeMap[projectType] === '日期'
+          ? 'DateRange'
+          : controlTypeMap[projectType] === '数值'
+          ? 'el-input-number'
+          : 'el-input'
+
+      forms.push({
+        ...(!index ? { title: '输入项' } : {}),
+        label: projectName,
+        key: projectName,
+        control,
+        ...(__isDev ? { value: 1 } : {})
+      })
     }
   })
+  dialog.forms = forms
   const link = ''
   dialog.action = {
     params: {},
@@ -38,7 +49,7 @@ export default (
       await trialPlan({
         versionId,
         salaryPlanId,
-        salaryCellItemBOSet: dialogFormData
+        ...dialogFormData
       })
     }
   }
--- shared/configurations/api-version.ts
+++ shared/configurations/api-version.ts
@@ -1,1 +1,1 @@
-export default 'yzl'
+export default 'kevin'
