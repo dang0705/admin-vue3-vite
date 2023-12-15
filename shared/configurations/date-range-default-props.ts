4e4b2705470ee26deba57187daf0282a57c88104
yujie <407278556@qq.com>
2023-12-15 11:49:02 +0800

    chore(shared/configurations/date-rage-default-props): Split the dateFormat

--- shared/configurations/date-range-default-props.ts
+++ shared/configurations/date-range-default-props.ts
@@ -1,6 +1,9 @@
+export const dateFormat = {
+  valueFormat: 'YYYY-MM-DD'
+}
 export default ({ startP = '开始', endP = '结束' } = {}) => ({
-	type: 'daterange',
-	startPlaceholder: startP,
-	endPlaceholder: endP,
-	valueFormat: 'YYYY-MM-DD',
-});
+  type: 'daterange',
+  startPlaceholder: startP,
+  endPlaceholder: endP,
+  valueFormat: 'YYYY-MM-DD'
+})
